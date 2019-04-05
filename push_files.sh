11da6270ffeec9a944a66de3ec3fed9fe7517509
Daniel Speer <dan.speer11@gmail.com>
2019-04-05 11:40:32 -0700

    add script to perturb zookeeper, script to fully restart kafka

--- push_files.sh
+++ push_files.sh
@@ -4,4 +4,5 @@ scp -i ~/.ssh/daniel-IAM-keypair.pem ./producer/s3_producer.py ubuntu@ec2-35-165
 #scp -i ~/.ssh/daniel-IAM-keypair.pem ./consumer/fake_consumer.py ubuntu@
 scp -i ~/.ssh/daniel-IAM-keypair.pem ./spark/consumer.py ubuntu@ec2-35-165-101-226.us-west-2.compute.amazonaws.com:~/
 scp -i ~/.ssh/daniel-IAM-keypair.pem ./spark/start_consumer.sh ubuntu@ec2-35-165-101-226.us-west-2.compute.amazonaws.com:~/
+scp -i ~/.ssh/daniel-IAM-keypair.pem ./kafka/perturb_zookeeper.sh ubuntu@ec2-52-39-96-200.us-west-2.compute.amazonaws.com:~/
 echo "upload complete"
