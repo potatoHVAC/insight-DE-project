34c19722fca288f7872d92ab0c3f1689d232f180
Daniel Speer <dan.speer11@gmail.com>
2019-04-07 18:50:32 -0700

    remove old project files that I have pivoted away from

--- kafka/perturb_zookeeper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ps aux | grep "zookeeper" | awk '{print $2}'
-sudo kill -9 $(ps aux | grep "zookeeper" | awk '{if(NR==2)print $2}')
-zkServer.sh start &
-sudo /usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties &
-ps aux | grep "zookeeper" | awk '{print $2}'
