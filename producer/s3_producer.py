da84f7db81105dfb0e4735841a0215887b56e61f
Daniel Speer <dan.speer11@gmail.com>
2019-04-08 14:15:23 -0700

    basic ddos detection enabled

--- producer/s3_producer.py
+++ producer/s3_producer.py
@@ -6,7 +6,12 @@ from smart_open import smart_open
 
 KAFKA_BUCKET = 's3://speerd-bucket/apache_logs.txt'
 KAFKA_TOPIC = 'apache_logs'
-KAFKA_BROKERS = 'ec2-34-215-153-129.us-west-2.compute.amazonaws.com:9092,ec2-52-36-50-195.us-west-2.compute.amazonaws.com:9092,ec2-52-88-169-142.us-west-2.compute.amazonaws.com:9092,ec2-54-71-226-161.us-west-2.compute.amazonaws.com:9092'
+KAFKA_BROKERS = (
+    'ec2-34-215-153-129.us-west-2.compute.amazonaws.com:9092,\
+    ec2-52-36-50-195.us-west-2.compute.amazonaws.com:9092,\
+    ec2-52-88-169-142.us-west-2.compute.amazonaws.com:9092,\
+    ec2-54-71-226-161.us-west-2.compute.amazonaws.com:9092'
+)
 
 def produce(bucket, brokers, topic):
 
--- spark/consumer.py
+++ spark/consumer.py
@@ -5,42 +5,87 @@ import os
 import re
 import sys
 import psycopg2
-from time import sleep
+import datetime
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
+from math import floor
 
 SPARK_MASTER = 'ec2-35-165-101-226.us-west-2.compute.amazonaws.com'
 APPNAME = 'SawThing'
-KAFKA_BROKERS = 'ec2-34-215-153-129.us-west-2.compute.amazonaws.com:9092,ec2-52-36-50-195.us-west-2.compute.amazonaws.com:9092,ec2-52-88-169-142.us-west-2.compute.amazonaws.com:9092,ec2-54-71-226-161.us-west-2.compute.amazonaws.com:9092'
+KAFKA_BROKERS = (
+    'ec2-34-215-153-129.us-west-2.compute.amazonaws.com:9092,\
+    ec2-52-36-50-195.us-west-2.compute.amazonaws.com:9092,\
+    ec2-52-88-169-142.us-west-2.compute.amazonaws.com:9092,\
+    ec2-54-71-226-161.us-west-2.compute.amazonaws.com:9092'
+)
 POSTGRESQL_URL = 'ec2-34-220-244-192.us-west-2.compute.amazonaws.com'
 POSTGRES_USER = os.environ['POSTGRES_USER']
 POSTGRES_PASS = os.environ['POSTGRES_PASS']
 DATABASE = 'menagerie'
-TOKEN_MAX = 100
+CREDITS_MAX = 30
 
 def connect_to_menagerie():        
-    conn = psycopg2.connect(host = POSTGRESQL_URL,
-                            database = DATABASE,
-                            user = POSTGRES_USER,
-                            password = POSTGRES_PASS)
+    conn = psycopg2.connect(
+        host = POSTGRESQL_URL,
+        database = DATABASE,
+        user = POSTGRES_USER,
+        password = POSTGRES_PASS
+    )
     return (conn.cursor(), conn)
 
-def get_ip(log):
-    return re.match('^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', log).group(0)
+def get_ip(raw_log):
+    return re.findall('^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', raw_log)[0]
 
-def get_date(log):
-    return re.findall('\d{2}\/\w{3}\/\d{4}:\d{2}:\d{2}:\d{2}', log)[0]
+def get_time_stamp(raw_log):
+    time_stamp_string = re.findall('\d{2}\/\w{3}\/\d{4}:\d{2}:\d{2}:\d{2}', raw_log)[0]
+    return datetime.datetime.strptime(time_stamp_string, '%d/%b/%Y:%H:%M:%S')
 
-def saw_menagerie_insert(line):
+def update_or_create_ip_entry(ip, time_stamp, cur):
+    cur.execute('SELECT credits, last_event FROM ip WHERE ip = %s', [ip])
+    ip_row = cur.fetchall()
+    
+    if len(ip_row) == 0:
+        cur.execute('INSERT INTO ip (ip, credits, last_event) values (%s, %s, %s);',
+                    [ip, CREDITS_MAX, time_stamp])
+        return
+    elif blacklisted_ip(ip, time_stamp, cur):
+        return
+    else:
+        cur.execute('SELECT credits, last_event FROM ip WHERE ip = %s;', [ip])
+        ip_credits, last_event = cur.fetchall()[0]
+        update_ip_credits(ip, time_stamp, ip_credits, last_event, cur)
+
+def blacklisted_ip(ip, time_stamp, cur, add_to_list = False):
+    cur.execute('SELECT * FROM blacklist WHERE ip = %s;', [ip])
+    blacklist_row = cur.fetchall()
+
+    if len(blacklist_row) == 0:
+        if add_to_list:
+            cur.execute('UPDATE ip SET credits = -3600, last_event = %s WHERE ip = %s;', [time_stamp, ip])
+            cur.execute('INSERT INTO blacklist(ip, last_event) VALUES (%s, %s);', [ip, time_stamp])
+        else:
+            return False
+    else:
+        cur.execute('UPDATE blacklist SET last_event = %s WHERE ip = %s;', [time_stamp, ip])
+        return True
+        
+def update_ip_credits(ip, time_stamp, ip_credits, last_event, cur):
+    delta_time_sec = int(floor((time_stamp - last_event).total_seconds()))
+    new_credits = max(-3600, min(ip_credits - 1 + (delta_time_sec // 2), CREDITS_MAX))
+
+    if new_credits <= 0:
+        blacklisted_ip(ip, time_stamp, cur, True)
+    else:
+        cur.execute('UPDATE ip SET credits = %s, last_event = %s WHERE ip = %s;', [new_credits, time_stamp, ip])
+
+def saw_database_check(line):
     cur, conn = connect_to_menagerie()
     for row in line:
         ip = get_ip(row[0])
-        print(ip)
-        date = get_date(row[0])
-        print(date)
-        #cur.execute(sql)
-        #conn.commit()
+        time_stamp = get_time_stamp(row[0])
+        update_or_create_ip_entry(ip, time_stamp, cur)
+        conn.commit()
     cur.close()
     conn.close()
 
@@ -51,7 +96,7 @@ def saw_main(sc, ssc):
         {'metadata.broker.list': KAFKA_BROKERS}
     )
     transactionSaw = kafkaStreamSaw.map(lambda row: row[1].split(','))
-    transactionSaw.foreachRDD(lambda rdd: rdd.foreachPartition(saw_menagerie_insert))
+    transactionSaw.foreachRDD(lambda rdd: rdd.foreachPartition(saw_database_check))
     
 def main():
     sc = SparkContext(appName = APPNAME)
