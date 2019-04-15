08d569a53137c4c382d6e74aa45f5363dfdbaa1f
Daniel Speer <dan.speer11@gmail.com>
2019-04-15 09:12:08 -0700

    update consumer names to more descriptive titles

--- push_files.sh
+++ push_files.sh
@@ -8,8 +8,10 @@ scp -i ~/.ssh/daniel-IAM-keypair.pem ./app/producer-node/ip_methods.py ubuntu@ec
 # kafka
 
 # spark
-scp -i ~/.ssh/daniel-IAM-keypair.pem ./data-processing/consumer.py ubuntu@ec2-35-165-101-226.us-west-2.compute.amazonaws.com:~/
-scp -i ~/.ssh/daniel-IAM-keypair.pem ./data-processing/start_consumer.sh ubuntu@ec2-35-165-101-226.us-west-2.compute.amazonaws.com:~/
+scp -i ~/.ssh/daniel-IAM-keypair.pem ./data-processing/postgresql_consumer.py ubuntu@ec2-35-165-101-226.us-west-2.compute.amazonaws.com:~/
+scp -i ~/.ssh/daniel-IAM-keypair.pem ./data-processing/postgresql_start_consumer.sh ubuntu@ec2-35-165-101-226.us-west-2.compute.amazonaws.com:~/
+scp -i ~/.ssh/daniel-IAM-keypair.pem ./data-processing/redis_consumer.py ubuntu@ec2-35-165-101-226.us-west-2.compute.amazonaws.com:~/
+scp -i ~/.ssh/daniel-IAM-keypair.pem ./data-processing/redis_start_consumer.sh ubuntu@ec2-35-165-101-226.us-west-2.compute.amazonaws.com:~/
 
 # database
 
