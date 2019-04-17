1a49ac04ec1fc65b3164b858a4dbefdc9c0074ff
Daniel Speer <dan.speer11@gmail.com>
2019-04-17 07:18:13 -0700

    change blacklist to flag for more appropriate nomenclature. removed unused variables.

--- data-processing/redis_consumer.py
+++ data-processing/redis_consumer.py
@@ -16,12 +16,11 @@ from variable_library import (
     APPNAME,
     KAFKA_BROKERS,
     DATABASE_IP,
-    MAIN_KAFKA_TOPIC
+    MAIN_KAFKA_TOPIC,
+    OLORIN_ASCII_LOGO
 )
 
 TOKENS_MAX = 100
-BLACKLIST_TIMER = 120
-TOKENS_TIMER = 120
 
 def connect_to_redis():
     return redis.Redis(
@@ -51,15 +50,15 @@ def send_logs_to_kafka(input_log, output_log, producer):
 def format_log_for_visualizer(log):
     return re.sub(' 429 ', ' 200 ', log)
 
-def blacklist_check(ip, red):
-    return red.get("blacklist{}".format(ip))
+def flag_check(ip, red):
+    return red.get("flag{}".format(ip))
 
-def blacklist_set(ip, time_stamp_sec, red):
-    red.set("blacklist{}".format(ip), time_stamp_sec)
+def flag_set(ip, time_stamp_sec, red):
+    red.set("flag{}".format(ip), time_stamp_sec)
 
 def tokens_set(ip, tokens, time_stamp, red):
     redis_value = "{},{}".format(tokens, time_stamp)
-    red.set(ip, redis_value, TOKENS_TIMER)
+    red.set(ip, redis_value)
 
 def update_tokens(tokens, last_time_stamp_sec, time_stamp_sec):
     new_token_value = tokens + time_stamp_sec - last_time_stamp_sec - 1
@@ -69,19 +68,22 @@ def check_or_update_ip_token(input_log, red):
     ip = get_ip(input_log)
     time_stamp_sec = convert_datetime_to_int(get_time_stamp(input_log))
     
-    if blacklist_check(ip, red):
-        blacklist_set(ip, time_stamp_sec, red)
+    if flag_check(ip, red):
+        flag_set(ip, time_stamp_sec, red)
         return format_log_for_visualizer(input_log)
 
+    # update tokens and time stamp, flag if tokens == 0
     try:
         tokens, last_time_stamp_sec = [ int(num) for num in red.get(ip).decode('utf-8').split(',') ]
         new_token_value = update_tokens(tokens, last_time_stamp_sec, time_stamp_sec)
         tokens_set(ip, new_token_value, time_stamp_sec, red)
 
         if new_token_value == 0:
-            blacklist_set(ip, time_stamp_sec, red)
+            flag_set(ip, time_stamp_sec, red)
             return format_log_for_visualizer(input_log)
-        return input_log        
+        return input_log
+    
+    # create token entry for unregistered ip
     except:
         tokens_set(ip, TOKENS_MAX // 5, time_stamp_sec, red)
         return input_log        
@@ -95,7 +97,7 @@ def olorin_database_check(line):
         output_log = check_or_update_ip_token(row[0], red)
         send_logs_to_kafka(row[0], output_log, producer)
 
-def olorin_main(sc, ssc):
+def olorin_main(ssc):
     kafkaStreamSaw = KafkaUtils.createDirectStream(
         ssc,
         [MAIN_KAFKA_TOPIC],
@@ -109,26 +111,9 @@ def main():
     sc.setLogLevel("WARN")
     ssc = StreamingContext(sc, 1)
 
-    print(r''' 
+    print(OLORIN_ASCII_LOGO)
 
-
-
-
-
-                    ________  .__               .__        
-                    \_____  \ |  |   __/________|__| ____  
-                     /   |   \|  |  /  _ \_  __ \  |/    \ 
-                    /    |    \  |_(  <_> )  | \/  |   |  \
-                    \_______  /____/\____/|__|  |__|___|  /
-                            \/                          \/ 
-
-
-
-
-
-    ''')
-
-    olorin_main(sc, ssc)
+    olorin_main(ssc)
     ssc.start()
     ssc.awaitTermination()
 
