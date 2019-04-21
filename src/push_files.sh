2392360cfdc7baaaa59940a940d6f76f4dbe0dcc
Daniel Speer <dan.speer11@gmail.com>
2019-04-20 17:07:03 -0700

    change relative path calls to direct using bash variables. now inlcude OLORIN_HOME, KAFKA_MASTER_IP, SPARK_MASTER_IP, PRODUCER_IP, DATABASE_IP, and VISUALIZER_IP

--- src/push_files.sh
+++ src/push_files.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # producer 
-scp -i ~/.ssh/daniel-IAM-keypair.pem ../olorin/producer-node/friendly_producer.py ubuntu@ec2-35-165-94-127.us-west-2.compute.amazonaws.com:~/
-scp -i ~/.ssh/daniel-IAM-keypair.pem ../olorin/producer-node/ddos_producer.py ubuntu@ec2-35-165-94-127.us-west-2.compute.amazonaws.com:~/
-scp -i ~/.ssh/daniel-IAM-keypair.pem ../olorin/producer-node/ip_methods.py ubuntu@ec2-35-165-94-127.us-west-2.compute.amazonaws.com:~/
+#scp -i ~/.ssh/daniel-IAM-keypair.pem ${OLORIN_HOME}/olorin/producer-node/friendly_producer.py ubuntu@
+#scp -i ~/.ssh/daniel-IAM-keypair.pem ${OLORIN_HOME}/olorin/producer-node/ddos_producer.py ubuntu@
+#scp -i ~/.ssh/daniel-IAM-keypair.pem ${OLORIN_HOME}/olorin/producer-node/ip_methods.py ubuntu@
 
 # kafka
 
 # spark
-scp -i ~/.ssh/daniel-IAM-keypair.pem ../olorin/spark-node/postgresql_consumer.py ubuntu@ec2-35-165-101-226.us-west-2.compute.amazonaws.com:~/
-scp -i ~/.ssh/daniel-IAM-keypair.pem ../olorin/spark-node/postgresql_start_consumer.sh ubuntu@ec2-35-165-101-226.us-west-2.compute.amazonaws.com:~/
-scp -i ~/.ssh/daniel-IAM-keypair.pem ../olorin/spark-node/redis_consumer.py ubuntu@ec2-35-165-101-226.us-west-2.compute.amazonaws.com:~/
-scp -i ~/.ssh/daniel-IAM-keypair.pem ../olorin/spark-node/redis_start_consumer.sh ubuntu@ec2-35-165-101-226.us-west-2.compute.amazonaws.com:~/
+#scp -i ~/.ssh/daniel-IAM-keypair.pem ${OLORIN_HOME}/olorin/spark-node/postgresql_consumer.py ubuntu@${SPARK_MASTER_IP}:~/
+#scp -i ~/.ssh/daniel-IAM-keypair.pem ${OLORIN_HOME}/olorin/spark-node/postgresql_start_consumer.sh ubuntu@${SPARK_MASTER_IP}:~/
+#scp -i ~/.ssh/daniel-IAM-keypair.pem ${OLORIN_HOME}/olorin/spark-node/redis_consumer.py ubuntu@${SPARK_MASTER_IP}:~/
+#scp -i ~/.ssh/daniel-IAM-keypair.pem ${OLORIN_HOME}/olorin/spark-node/redis_start_consumer.sh ubuntu@${SPARK_MASTER_IP}:~/
 
 # database
 
 # visualizer
 
-scp -i ~/.ssh/daniel-IAM-keypair.pem ../olorin/visualizer-node/olorin.py ubuntu@ec2-34-218-254-7.us-west-2.compute.amazonaws.com:~/
-scp -i ~/.ssh/daniel-IAM-keypair.pem ../olorin/visualizer-node/input_consumer.py ubuntu@ec2-34-218-254-7.us-west-2.compute.amazonaws.com:~/
-scp -i ~/.ssh/daniel-IAM-keypair.pem ../olorin/visualizer-node/output_consumer.py ubuntu@ec2-34-218-254-7.us-west-2.compute.amazonaws.com:~/
-scp -i ~/.ssh/daniel-IAM-keypair.pem ../olorin/visualizer-node/consumer_methods.py ubuntu@ec2-34-218-254-7.us-west-2.compute.amazonaws.com:~/
-scp -i ~/.ssh/daniel-IAM-keypair.pem ../olorin/visualizer-node/ip_consumer.py ubuntu@ec2-34-218-254-7.us-west-2.compute.amazonaws.com:~/
+#scp -i ~/.ssh/daniel-IAM-keypair.pem ${OLORIN_HOME}/olorin/visualizer-node/olorin.py ubuntu@${VISUALIZER_IP}:~/
+#scp -i ~/.ssh/daniel-IAM-keypair.pem ${OLORIN_HOME}/olorin/visualizer-node/input_consumer.py ubuntu@${VISUALIZER_IP}:~/
+#scp -i ~/.ssh/daniel-IAM-keypair.pem ${OLORIN_HOME}/olorin/visualizer-node/output_consumer.py ubuntu@${VISUALIZER_IP}:~/
+#scp -i ~/.ssh/daniel-IAM-keypair.pem ${OLORIN_HOME}/olorin/visualizer-node/consumer_methods.py ubuntu@${VISUALIZER_IP}:~/
+#scp -i ~/.ssh/daniel-IAM-keypair.pem ${OLORIN_HOME}/olorin/visualizer-node/ip_consumer.py ubuntu@${VISUALIZER_IP}:~/
--- src/variable_push.sh
+++ src/variable_push.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # producer 
-scp -i ~/.ssh/daniel-IAM-keypair.pem ../variable_library/variable_library.py ubuntu@ec2-35-165-94-127.us-west-2.compute.amazonaws.com:~/
+scp -i ~/.ssh/daniel-IAM-keypair.pem ${OLORIN_HOME}/variable_library/variable_library.py ubuntu@${PRODUCER_IP}:~/
 
 # kafka
 
 # spark
-scp -i ~/.ssh/daniel-IAM-keypair.pem ../variable_library/variable_library.py ubuntu@ec2-35-165-101-226.us-west-2.compute.amazonaws.com:~/
-scp -i ~/.ssh/daniel-IAM-keypair.pem ../variable_library/private_variable_library.py ubuntu@ec2-35-165-101-226.us-west-2.compute.amazonaws.com:~/
+scp -i ~/.ssh/daniel-IAM-keypair.pem ${OLORIN_HOME}/variable_library/variable_library.py ubuntu@${SPARK_MASTER_IP}:~/
+scp -i ~/.ssh/daniel-IAM-keypair.pem ${OLORIN_HOME}/variable_library/private_variable_library.py ubuntu@${SPARK_MASTER_IP}:~/
 
 # database
 
-# dash
+# visualizer
 
-scp -i ~/.ssh/daniel-IAM-keypair.pem ../variable_library/variable_library.py ubuntu@ec2-34-218-254-7.us-west-2.compute.amazonaws.com:~/
+scp -i ~/.ssh/daniel-IAM-keypair.pem ${OLORIN_HOME}/variable_library/variable_library.py ubuntu@${VISUALIZER_IP}:~/
