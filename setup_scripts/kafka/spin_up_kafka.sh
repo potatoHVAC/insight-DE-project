77df4ecb79b516e6a7fb5e99f956b9c776b260c1
Daniel Speer <dan.speer11@gmail.com>
2019-04-21 23:40:39 -0700

    fix kafka install bug

--- setup_scripts/kafka/spin_up_kafka.sh
+++ setup_scripts/kafka/spin_up_kafka.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#peg up master.yml
-#peg up workers.yml
-#peg fetch kafka-cluster
-#peg sshcmd-cluster kafka-cluster "sudo apt upgrade -y; sudo apt install bc -y;"
+peg up master.yml
+peg up workers.yml
+peg fetch kafka-cluster
+peg sshcmd-cluster kafka-cluster "sudo apt install bc;"
 peg install kafka-cluster ssh
 peg install kafka-cluster aws
 peg install kafka-cluster environment
